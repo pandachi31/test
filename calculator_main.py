b6b3cb8bd6609935cd001f9f17af0be3fd8dabc2
Tea Hoon <dkdkdk@example.com>
2022-11-16 18:47:53 +0900

    새버튼 추가 #10

--- calculator_main.py
+++ calculator_main.py
@@ -124,7 +124,7 @@ class Main(QDialog):
         button_dot.clicked.connect(lambda state, num = ".": self.number_button_clicked(num))
         layout_number.addWidget(button_dot, 5, 2)
 
-        button_double_zero = QPushButton("00")
+        button_double_zero = QPushButton("+/-")
         button_double_zero.clicked.connect(lambda state, num = "00": self.number_button_clicked(num))
         layout_number.addWidget(button_double_zero, 5, 0)
 
